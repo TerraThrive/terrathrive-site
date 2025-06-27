24ad83421dca4c4ee2a93257674a9c4b5d56a8e1
TerraThrive <terrathrivellp@gmail.com>
2025-06-27 20:42:18 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./index.html", "./main.jsx"],
+  theme: {
+    extend: {
+      colors: {
+        beige: '#f5f0e6',
+        brown: '#4b3621',
+        green: {
+          700: '#4b8b3b'
+        },
+        orange: {
+          700: '#c75000'
+        }
+      },
+      fontFamily: {
+        serif: ['Georgia', 'serif'],
+        sans: ['Helvetica', 'Arial', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
